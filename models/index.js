90e3d343525b93c53dbcd94349506c983d12fbd0
Dipankar <bhoumik.dipu@gmail.com>
2022-04-22 23:42:15 +0530

    adding database for production

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/db.config.json');
-const env = "development";
+const env = process.env.NODE_ENV ||  "development";
 const dbSetting = dbConfig[env];
 const sequelize = new Sequelize(
     dbSetting.database,
